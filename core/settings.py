553ec77166fd9f264977ad915e4e7bfd35bbf6e6
Kaique <motamendes.kaique.100@gmail.com>
2022-09-12 07:42:34 -0300

    adding allow all origins as true

--- core/settings.py
+++ core/settings.py
@@ -152,8 +152,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
-]
+CORS_ORIGIN_ALLOW_ALL = True
